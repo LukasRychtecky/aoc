eaddd5c2c93bc9644632a4242386f1275458c7c4
Lukáš Rychtecký <lukas.rychtecky@applifting.cz>
2024-12-05 12:12:30 +0100

    Add a solution for AoC24 day 1

--- aoc24/src/aoc24/core.clj
+++ aoc24/src/aoc24/core.clj
@@ -1,6 +1,40 @@
 (ns aoc24.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn transpose
+  [m]
+  (apply mapv vector m))
+
+(defn parse-input-file
+  [cols url]
+  (let [content
+        (-> url
+            slurp
+            (clojure.string/split #"\s+"))]
+    (->> content
+         (map parse-long)
+         (partition-all cols)
+         transpose)))
+
+(defn day1a
+  [url]
+  (let [cols (parse-input-file 2 url)]
+    (->> cols
+         (map sort)
+         (cons (comp abs -))
+         (apply map)
+         (reduce + 0))))
+
+(defn day1b
+  [url]
+  (let [[xs ys]
+        (parse-input-file 2 url)]
+    (->> xs
+         set
+         (select-keys (frequencies ys))
+         (map (partial apply *))
+         (reduce + 0))))
+
+(comment
+  (let [file "resources/aoc24-input.txt"]
+    ;; (day1a file)
+    (day1b file)))
